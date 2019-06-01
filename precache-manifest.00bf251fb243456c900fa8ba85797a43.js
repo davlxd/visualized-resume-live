925c490df10ee931bbe17f1326fb5316f2196d37
lxd <i@lxd.me>
2019-06-01 21:12:23 +1200

    Release

---
+++ precache-manifest.00bf251fb243456c900fa8ba85797a43.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc614e58b05dcec45e8ae094e3d6959",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e965cef31c0d72fe4de",
+    "url": "/static/js/2.50b30776.chunk.js"
+  },
+  {
+    "revision": "d4355fb8dc0ddbf82693",
+    "url": "/static/js/main.6a1f4f47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
